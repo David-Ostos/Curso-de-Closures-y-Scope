2018401f73f7942b405b38d3ff7ac5d7abb8d0b8
David Ostos <davidostos2@gmail.com.com>
2023-02-16 20:24:46 -0400

    agregando los canvios del archivo strick

--- src/scope/strict.js
+++ src/scope/strict.js
@@ -1,10 +1,19 @@
-'use strict';
+'use strict'; // asi se declara las restrinciones 
 pi = 3.1416;
 console.log(pi);
 
-function funcion(){
+function funcion(){ // esta funcione funciona ya que no esta estricta y 
+                    // y aunque no este declarada funciona
+    return pi= 3.1416;
+}
+
+console.log(funcion());
+
+function funcion(){// esta funcion no esta permitida
     'use strict';
     return pi= 3.1416;
 }
 
 console.log(funcion());
+
+
